3b6ed7f5983e27842094758ec0473645e3c2f034
Kidyoh <kidyoh789@gmail.com>
2023-07-27 12:56:21 +0300

    Data fetching for json and xml is done

--- auto.py
+++ auto.py
@@ -2,56 +2,123 @@ import os
 import requests
 import pandas as pd
 import urllib.parse
-from aspose.cells import Cells
+import xml.parsers.expat
+import xmltodict
 from aspose.cells import Workbook
-import re
-from ms_graph import generate_access_token, GRAPH_API_ENDPOINT
+import json
 
-APP_ID = "8c31f0a0-26ca-42d2-99fd-e2d8d2a6b08d"
-SCOPES = ['Files.ReadWrite']
+# Function to remove characters before the <?xml version declaration
+def remove_invalid_characters(xml_data):
+    xml_declaration = '<?xml version'
+    start_index = xml_data.find(xml_declaration)
+    if start_index != -1:
+        cleaned_data = xml_data[start_index:]
+        return cleaned_data
+    else:
+        print("Invalid XML data format.")
+        return None
 
-access_token = generate_access_token(APP_ID, SCOPES)
-headers = {
-   'Authorization' : 'Bearer' + access_token['access_token']
-   
-}
+# Function to convert XML to JSON
+def xml_to_json(xml_string):
+    try:
+        data_dict = xmltodict.parse(xml_string)
+        json_data = json.dumps(data_dict)
+        return json_data
+    except xml.parsers.expat.ExpatError as e:
+        print(f"Error parsing XML: {e}")
+        return None
 
-# Read Excel file
-df = pd.read_excel('indicators.xlsx')
+def save_xml_data(xml_file, xml_data):
+    with open(xml_file, "w", encoding="utf-8") as f:
+        f.write(xml_data)
 
-indicators = df['Indicator'].tolist()
+def flatten(d, parent_key=''):
+    items = []
+    if isinstance(d, dict):
+        for k, v in d.items():
+            new_key = f"{parent_key}_{k}" if parent_key else k   
+            if isinstance(v, dict):
+                items.extend(flatten(v, new_key).items())
+            else:
+                v = [v] # convert to list
+                items.append((new_key, v))
+    elif isinstance(d, list):
+        for i, v in enumerate(d):
+            new_key = f"{parent_key}_{i}"    
+            if isinstance(v, dict):
+                items.extend(flatten(v, new_key).items())
+            else:
+                v = [v] # convert to list
+                items.append((new_key, v))
+    return dict(items)
 
+if __name__ == "__main__":
+    # Read Excel file
+    df = pd.read_excel('indicators.xlsx')
+    indicators = df['Indicator'].tolist()
 
-# Set country to Ethiopia
-country = 'ETH'
+    # Set country to Ethiopia
+    country = 'ETH'
 
-# Create a workbook to save data
-workbook = Workbook()
+    # Create a workbook to save data
+    workbook = Workbook()
 
-for indicator in indicators:
+    for indicator in indicators:
+        url = f"https://api.worldbank.org/countries/{country}/indicators/{indicator}"
+        parsed = urllib.parse.urlparse(url)
+        params = urllib.parse.parse_qs(parsed.query)
+        source = parsed.hostname  # worldbank.org
+        datapoint = parsed.path.split('/')[-1]  # population
+        root_dir = 'data'
+        source_dir = os.path.join(root_dir, source)
+        country_dir = os.path.join(source_dir, country)
+        datapoint_dir = os.path.join(country_dir, datapoint)
+        os.makedirs(datapoint_dir, exist_ok=True)
 
-    url = f"https://api.worldbank.org/countries/{country}/indicators/{indicator}"
-    parsed = urllib.parse.urlparse(url)
-    params = urllib.parse.parse_qs(parsed.query)
-    source = parsed.hostname  # worldbank.org
-    datapoint = parsed.path.split('/')[-1]  # population
-    root_dir = 'data'
-    source_dir = os.path.join(root_dir, source)
-    country_dir = os.path.join(source_dir, country)
-    datapoint_dir = os.path.join(country_dir, datapoint)
-    os.makedirs(datapoint_dir, exist_ok=True)
-    # Make API request
-    response = requests.get(url)
-    if(response.status_code == 200):
-      with open(os.path.join(datapoint_dir, f'{datapoint}.xml'), 'w') as f:
-        f.write(response.text)
-      with open(os.path.join(datapoint_dir, f'{datapoint}.xml'), 'w') as upload:
-         content = upload.read()
-requests.put(
-   GRAPH_API_ENDPOINT + f'/me/drive/root:/{datapoint}.xml:/content',
-)
+        # Make API request
+        response = requests.get(url)
+        if response.status_code == 200:
+            xml_data = response.text
+            # Remove characters before <?xml version declaration
+            xml_data_cleaned = remove_invalid_characters(xml_data)
+            if xml_data_cleaned:
+                # Save the cleaned XML data to the file (ensure well-formed XML)
+                xml_file = os.path.join(datapoint_dir, f'{datapoint}.xml')
+                save_xml_data(xml_file, xml_data_cleaned)
 
-print('Data saved to worksheet', indicator)
-# Save the entire workbook to the Excel file
-workbook.save("Output.xlsx")
-print('Excel file "Output.xlsx" saved.')
+                # Convert XML to JSON
+                json_data = xml_to_json(xml_data_cleaned)
+                if json_data:
+                    # Create DataFrame from JSON data
+                    df_json = pd.read_json(json_data)
+
+                    # Save the JSON data to an Excel file in a separate sheet for each country
+                    sheet_name = f"{country}_{datapoint}"
+                    file_path_json = os.path.join(datapoint_dir, f"{datapoint}.xlsx")
+                    with pd.ExcelWriter(file_path_json, engine='xlsxwriter') as writer:
+                        df_json.to_excel(writer, sheet_name=sheet_name, index=False)
+                        worksheet = writer.sheets[sheet_name]
+                        worksheet.autofilter(0, 0, df_json.shape[0], df_json.shape[1] - 1)  # Add autofilter
+                        worksheet.freeze_panes(1, 0)  # Freeze the top row
+                        worksheet.set_column('A:A', 30)  # Adjust column width
+
+                    print(f'JSON data saved to {file_path_json}')
+
+                    # Flatten data
+                    if isinstance(df_json, dict):
+                        flat_data = flatten(df_json)
+                    else:
+                        flat_data = flatten({"data": df_json})
+
+                    # Create DataFrame from the flattened data
+                    df_flat = pd.DataFrame.from_dict(flat_data)
+
+                    # Save the flattened data to an Excel file
+                    file_path_flat = os.path.join(datapoint_dir, f"{datapoint}_flat.xlsx")
+                    df_flat.to_excel(file_path_flat, index=False)
+
+                    print("Flattened data saved to", file_path_flat)
+
+    # Save the entire workbook to the Excel file
+    workbook.save("Output.xlsx")
+    print('Excel file "Output.xlsx" saved.')
---
+++ data_processionjs.py
@@ -0,0 +1,90 @@
+import urllib.parse
+import os
+import requests
+import json
+import pandas as pd
+
+
+url = input("Enter API URL: ")
+
+# url = "https://api.worldbank.org/pip/v1/pip?country=SSA&year=2008&povline=1.9&fill_gaps=false"
+
+parsed = urllib.parse.urlparse(url)
+params = urllib.parse.parse_qs(urllib.parse.urlparse(url).query)
+
+country = params['country'][0]
+source = parsed.hostname # worldbank.org
+datapoint = parsed.path.split('/')[-1] # population
+
+root_dir = 'new'
+source_dir = os.path.join(root_dir, source)
+country_dir = os.path.join(source_dir, country)
+datapoint_dir = os.path.join(country_dir, datapoint) 
+
+os.makedirs(datapoint_dir, exist_ok=True)
+
+
+# if country == 'ETH':
+  
+response = requests.get(url)
+data = response.json()
+
+with open(os.path.join(datapoint_dir, 'data.json'), 'w') as f:
+  json.dump(data, f)
+# Flatten function
+def flatten(d, parent_key=''):
+  
+  items = []
+  
+  if isinstance(d, dict):
+    for k, v in d.items():
+      new_key = f"{parent_key}_{k}" if parent_key else k   
+      if isinstance(v, dict):
+        items.extend(flatten(v, new_key).items())
+      else:
+        v = [v] # convert to list
+        items.append((new_key, v))
+        
+  elif isinstance(d, list):
+    for i, v in enumerate(d):
+      new_key = f"{parent_key}_{i}"    
+      if isinstance(v, dict):
+        items.extend(flatten(v, new_key).items())
+      else:
+        v = [v] # convert to list
+        items.append((new_key, v))
+
+  return dict(items)
+
+
+# Flatten data
+if isinstance(data, dict):
+    flat_data = flatten(data)
+else:
+    flat_data = flatten({"data": data})
+
+# Create DataFrame from the flattened data
+df_flat = pd.DataFrame.from_dict(flat_data)
+
+# Save the flattened data to an Excel file
+file_path_flat = os.path.join(datapoint_dir, f"{datapoint}_flat.xlsx")
+df_flat.to_excel(file_path_flat, index=False)
+
+print("Flattened data saved to", file_path_flat)
+
+# Create separate DataFrames for each country
+dfs_by_country = {}
+if isinstance(data, dict):
+    for country_code, country_data in data.items():
+        df = pd.json_normalize(country_data, meta='all')
+        dfs_by_country[country_code] = df
+else:
+    df = pd.json_normalize(data, meta='all')
+    dfs_by_country[country] = df
+
+# Save each DataFrame to a separate sheet in the Excel file
+with pd.ExcelWriter(os.path.join(datapoint_dir, f"{datapoint}_by_country.xlsx")) as writer:
+    for country_code, df in dfs_by_country.items():
+        df.to_excel(writer, sheet_name=country_code, index=False)
+
+print("Data saved by country to", os.path.join(datapoint_dir, f"{datapoint}_by_country.xlsx"))
